dd11710d83275c4b92463ea1add4db3e2eaa08c7
David Saifer <spyrotheultimate@gmail.com>
2025-07-07 23:33:50 -0700

    remove sorta working

--- script.js
+++ script.js
@@ -10,52 +10,99 @@ block_types = ["blood", "guts", "brain", "eyes"]
 
 let selected = undefined
 
-for (x = 0; x < width * height; x ++) {
-    block = document.createElement('div')
-    random_type = block_types[Math.floor(Math.random() * block_types.length)]
-    block.setAttribute("type", random_type)
-    block.classList.add(random_type)
-    grid.appendChild(block) 
+internal_grid = []
 
-    block.addEventListener('click', (e) => {
-        e.currentTarget.classList.add("selected");
-        if (selected == undefined)
-            selected = e.currentTarget
-        else {
-            try_swap(selected, e.currentTarget)
-        }
-    })
-
-    
+for (y = 0; y < height; y ++) {
+    internal_grid[y] = []
 }
 
-check_matches()
+function get_block(x, y) {
+    return internal_grid[y][x]
+}
+
+function set_block(x, y, block) {
+    internal_grid[y][x] = block
+    block.style.gridColumn = `${x + 1} / ${x + 1} `
+    block.style.gridRow = `${y + 1} / ${y + 1}`
+}
+
+function swap_blocks(x1, y1, x2, y2) {
+
+    hold = get_block(x1, y1)
+    set_block(x1, y1, get_block(x2, y2))
+    set_block(x2, y2, hold)
+
+}
+
+function get_coordinates(block) {
+    for (y = 0; y < height; y ++) {
+    
+        for (x = 0; x < width; x ++) {
+            if (internal_grid[y][x] == block)
+                return [x, y]
+        }
+    }
+    return undefined
+}
+
+
+function delete_block(x, y) {
+    internal_grid[y][x].remove()
+    delete internal_grid[y][x]
+
+}
+
+
+for (y = 0; y < height; y ++) {
+    
+    for (x = 0; x < width; x ++) {
+        
+        block = document.createElement('div')
+        random_type = block_types[Math.floor(Math.random() * block_types.length)]
+        block.setAttribute("type", random_type)
+        block.classList.add(random_type)
+        block.classList.add("block")
+        
+        set_block(x, y, block)
+        grid.appendChild(block) 
+
+        block.addEventListener('click', (e) => {
+            e.currentTarget.classList.add("selected");
+            if (selected == undefined)
+                selected = e.currentTarget
+            else {
+                try_swap(selected, e.currentTarget)
+            }
+        })
+
+    }
+}
 
 function try_swap(b1, b2) {
     b1.classList.remove("selected")
     b2.classList.remove("selected")
     selected = undefined
 
-    var b1index = Array.prototype.indexOf.call(grid.children, b1);
-    b1x = b1index % width
-    b1y = Math.floor(b1index / height)
+    var b1coord = get_coordinates(b1);
+    b1x = b1coord[0]
+    b1y = b1coord[1]
 
-    var b2index = Array.prototype.indexOf.call(grid.children, b2);
-    b2x = b2index % width
-    b2y = Math.floor(b2index / height)
+    var b2coord = get_coordinates(b2);
+    b2x = b2coord[0]
+    b2y = b2coord[1]
+
+    console.log(b1coord, b2coord)
         
 
     if ((Math.abs(b1x - b2x) <= 1) && (Math.abs(b1y - b2y) <= 1)) {
-        temp = document.createElement("div")
-        grid.insertBefore(temp, b2)
-        grid.insertBefore(b2, b1)
-        grid.insertBefore(b1, temp)
-
-        temp.remove()
+        
 
         if (check_matches()) {
+
+            
+
             remove_blocks()
-            add_blocks
+            add_blocks()
         }
         else {  
             temp = document.createElement("div")
@@ -74,43 +121,38 @@ function check_matches() {
 
     matches_flag = false
 
-    function get_block(x, y) {
-        if (x >= width) return undefined
-        if (y >= height) return undefined
-
-        return grid.children[x + y * width]
-    }
-
-    function get_block_attribute(x, y) {
-        if (x >= width) return undefined
-        if (y >= height) return undefined
-
-        return grid.children[x + y * width].getAttribute("type")
-    }
 
     for (x = 0; x < width; x ++) {
         for (y = 0; y < height; y ++) {
 
             
-            if ( get_block_attribute(x, y) == get_block_attribute(x + 1, y) &&
-                    get_block_attribute(x, y) == get_block_attribute(x + 2, y)) 
+            if (x < width - 2 && get_block(x, y).getAttribute("type") == get_block(x + 1, y).getAttribute("type") &&
+                    get_block(x, y).getAttribute("type") == get_block(x + 2, y).getAttribute("type")) 
               {
                 get_block(x, y).setAttribute("remove", "true");
                 get_block(x + 1, y).setAttribute("remove", "true");
                 get_block(x + 2, y).setAttribute("remove", "true");
 
+                get_block(x, y).classList.add("remove")
+                get_block(x + 1, y).classList.add("remove")
+                get_block(x + 2, y).classList.add("remove")
+
                 matches_flag = true
             }
 
 
-            if ( get_block_attribute(x, y) == get_block_attribute(x, y + 1) &&
-                    get_block_attribute(x, y) == get_block_attribute(x, y + 2)) 
+            if (y < height - 2 && get_block(x, y).getAttribute("type") == get_block(x, y + 1).getAttribute("type") &&
+                    get_block(x, y).getAttribute("type") == get_block(x, y + 2).getAttribute("type")) 
               {
 
 
                 get_block(x, y).setAttribute("remove", "true");
                 get_block(x, y + 1).setAttribute("remove", "true");
                 get_block(x, y + 2).setAttribute("remove", "true");
+
+                get_block(x, y).classList.add("remove")
+                get_block(x, y + 1).classList.add("remove")
+                get_block(x, y + 2).classList.add("remove")
 
                 matches_flag = true
             }
@@ -122,19 +164,24 @@ function check_matches() {
 }
 
 function remove_blocks() {
-    for (block of grid.children) {
-        console.log(block)
-        if (block.getAttribute("remove") == "true") {
-            block.remove()
+    
+    for (y = 0; y < height; y ++) {
+        
+        for (x = 0; x < width; x ++) {
+            console.log(get_block(x, y))
+
+            if (get_block(x, y).getAttribute("remove") == "true") {
+                delete_block(x, y)
+            }
         }
     }
 }
 
 function add_blocks() {
-    for (block of grid.children) {
-        console.log(block)
-        if (block.getAttribute("remove") == "true") {
-            block.remove()
-        }
-    }
+    // for (block of grid.children) {
+    //     console.log(block)
+    //     if (block.getAttribute("remove") == "true") {
+    //         block.remove()
+    //     }
+    // }
 }
